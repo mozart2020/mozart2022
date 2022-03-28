281e0f611c5b85064addaa2083284bc3b18563fd
Helmut Gugerbauer <h.gugerbauer@gmail.com>
2022-03-28 20:29:34 +0200

    Basic Video Recorder

--- src/app/content/add-video/add-video.page.ts
+++ src/app/content/add-video/add-video.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { VideoService } from 'src/app/services/video.service';
 
 @Component({
   selector: 'app-add-video',
@@ -6,10 +8,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-video.page.scss'],
 })
 export class AddVideoPage implements OnInit {
+  currentUserImage: string;
+  @ViewChild('video') captureElement: ElementRef;
+  mediaRecorder: MediaRecorder;
+  videoPlayer: any;
+  isRecording = false;
+  videos = [];
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private videoService: VideoService) {
+
+  }
 
   ngOnInit() {
+    this.authService.getCurrentUser().subscribe(res => {
+      this.currentUserImage = res.profileImage;
+    });
+  }
+
+  async recordVideo() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'user'
+      },
+      audio: true
+    });
+    this.captureElement.nativeElement.srcObject = stream;
+    this.isRecording = true;
+    //Recorder:
+    const options = { mimeType: 'video/webm' };
+    this.mediaRecorder = new MediaRecorder(stream, options);
+    let chunks = [];
+    this.mediaRecorder.ondataavailable = (event) => {
+      if (event.data && event.data.size > 0) {
+        chunks.push(event.data)
+      }
+    }
+    this.mediaRecorder.start(100);
+    this.mediaRecorder.onstop = async (event) => {
+      const videoBuffer = new Blob(chunks, { type: 'video/webm' });
+      //await this.videoService.storeVideo(videoBuffer);
+      
+      // Reload our list
+      //this.videos = this.videoService.videos;
+      //this.changeDetector.detectChanges();
+    }
+
+  }
+
+  stopRecord() {
+    this.mediaRecorder.stop();
+    this.mediaRecorder = null;
+    this.captureElement.nativeElement.srcObject = null;
+    this.isRecording = false;
+  }
+
+  async playVideo() {
+
   }
 
 }
